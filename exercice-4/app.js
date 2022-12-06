9db537af1b212d090b79656307469e5e9cd6915d
Steven Lemasson <lemasson.steven@gmail.com>
2022-12-06 11:19:34 +0100

    fourth exercice done

--- exercice-4/app.js
+++ exercice-4/app.js
@@ -1,1 +1,25 @@
-console.log('bonjour')
+class Human {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+const donald = new Human('Donald', 'Duck');
+console.log(`Bonjour ${donald.firstName} ${donald.lastName}`);
+
+class Child extends Human {
+    constructor(firstName, lastName) {
+        super(firstName, lastName);
+        this.child = true;
+    }
+
+    play() {
+        console.log(`Je m'apelle ${this.firstName} et je suis en train de jouer`);
+    }
+}
+
+const riri = new Child('Riri', 'Duck');
+if(riri.child) {
+    riri.play();
+}
