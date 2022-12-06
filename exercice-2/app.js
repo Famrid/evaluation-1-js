65028a574716332859f9e5809a631845d5b0623f
Steven Lemasson <lemasson.steven@gmail.com>
2022-12-06 11:02:05 +0100

    second ecercice done

--- exercice-2/app.js
+++ exercice-2/app.js
@@ -1,1 +1,19 @@
-console.log('bonjour')
+function hello () {
+    console.log('Hello world');
+}
+hello();
+
+function helloSomeone (name) {
+    console.log(`Bonjour ${name}`);
+}
+helloSomeone('fifi');
+
+function helloSomeoneInUppercase (name) {
+    console.log(`Bonjour ${name.toUpperCase()}`);
+}
+helloSomeoneInUppercase('loulou');
+
+function helloSomeoneInCapitalize (name) {
+    console.log(`Bonjour ${name.charAt(0).toUpperCase()+name.slice(1)}`);
+}
+helloSomeoneInCapitalize('picsou');
